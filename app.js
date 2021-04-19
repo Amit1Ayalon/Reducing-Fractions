1619759b9bb938bf2396c1b54814eea14aa9f206
Amit Ayalon <ayalon1amit@gmail.com>
2021-04-19 12:56:49 +0300

    Added isPrime and findPrime functions

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("this is working!");
+//setting the inital numerator and denominator
+const initialNumerator = 3;
+const initialDenominator = 6;
+
+function findPrime(stopNum) {
+    let primelist = [];
+    for (let i = 1; i <= stopNum; i++) {
+        if (isPrime(i)) primelist.push(i);
+    }
+    console.log(primelist);
+}
+
+function isPrime(num) {
+    for (let i = 2; i < num; i++)
+        if (num % i === 0) return false;
+    return num >= 1;
+}
+
+
+findPrime(111);
