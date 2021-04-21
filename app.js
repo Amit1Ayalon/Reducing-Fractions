dbec2964d5635a342b77bb7ca046ddcd557437b8
Amit Ayalon <ayalon1amit@gmail.com>
2021-04-21 09:47:05 +0300

    Implementing the whole "reducing fraction" process

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
 //setting the inital numerator and denominator
-const initialNumerator = 3;
-const initialDenominator = 6;
+const initialNumerator = 4;
+const initialDenominator = 16;
 
-function findPrime(stopNum) {
-    let primelist = [];
-    for (let i = 1; i <= stopNum; i++) {
-        if (isPrime(i)) primelist.push(i);
+
+function reduceMasterFunction(numerator, denominator) {
+    let stopReduce = false;
+    let n = numerator;
+    let d = denominator;
+    while (true) {
+        if (stopReduce) break;
+
+        fraction = reduceFraction(n, d);
+
+        if (fraction[0] === n && fraction[1] === d) stopReduce = true;
+
+        n = fraction[0];
+        d = fraction[1];
     }
-    console.log(primelist);
+
+    console.log("numerator: " + n);
+    console.log("denominator: " + d);
+}
+
+
+function reduceFraction(n, d) {
+    let primes;
+    let commonPrime;
+    if (n < d) {
+        primes = findPrimes(n);
+        commonPrime = FindPrimeMatch(primes, d);
+    } else {
+        primes = findPrimes(d);
+        commonPrime = FindPrimeMatch(primes, n);
+    }
+
+    if (commonPrime != null) {
+        n /= commonPrime;
+        d /= commonPrime;
+    }
+    return [n, d];
+}
+
+function findPrimes(num) {
+    let primesList = [];
+    for (let i = 1; i <= num; i++) {
+        if (isPrime(i) && num % i === 0) primesList.push(i);
+    }
+    return primesList;
 }
 
 function isPrime(num) {
     for (let i = 2; i < num; i++)
         if (num % i === 0) return false;
-    return num >= 1;
+    return num > 1;
+}
+
+function FindPrimeMatch(primesList, numToMatch) {
+    for (let i = 0; i < primesList.length; i++) {
+        if (numToMatch % primesList[i] === 0) return primesList[i];
+    }
+    return null
 }
 
 
-findPrime(111);
+reduceMasterFunction(initialNumerator, initialDenominator);
